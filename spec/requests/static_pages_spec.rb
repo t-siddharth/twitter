ac7e29d007a7f238b8c48df87601d7c93ee0d522
Sidd <sidtewari@gmail.com>
2013-05-03 01:24:58 -0400

    Tests added - failed and then passed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title" do
+    	visit '/static_pages/home'
+    	page.should have_selector('title', :text => "Ruby on Rails Twitter Sample App | Home")
+    end
   end
 
   describe "Help Page" do
@@ -13,12 +18,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+    	visit '/static_pages/help'
+    	page.should have_selector('title', :text => "Ruby on Rails Twitter Sample App | Help")
+    end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+    	visit '/static_pages/about'
+    	page.should have_selector('title', :text => "Ruby on Rails Twitter Sample App | About")
     end
   end
 
