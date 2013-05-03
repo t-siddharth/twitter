087b29b35ca8a1379432d83f7dda8f6f4c4746b8
Sidd <sidtewari@gmail.com>
2013-05-03 00:52:36 -0400

    Basic test added for About Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,17 @@ describe "StaticPages" do
   end
 
   describe "Help Page" do
-    it "should have the content 'Help Page'" do
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help Page')
+      page.should have_content('Help')
     end
   end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
 end
