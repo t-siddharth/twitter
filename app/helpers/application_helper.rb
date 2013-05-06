eb89d73895a1fa120a61965e72282773c9839b4c
Sidd <sidtewari@gmail.com>
2013-05-06 14:41:07 -0400

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Returns the full page title
+	def full_title(page_title)
+		base_title = "Ruby on Rails Twitter Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
